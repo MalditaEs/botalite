ceee1d5978b6f888547a9642e9087370148856c0
malditomas <tramirez@maldita.es>
2023-06-08 11:20:02 +0200

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/app.js",
+    "./templates/index.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
