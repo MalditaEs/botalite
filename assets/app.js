d2afc52fd840d5ba3ebb924ddad8c5065c180880
Tomas Ramirez <tramirez@maldita.es>
2023-06-20 16:59:51 +0200

    The desktop version of the landing is done.

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
