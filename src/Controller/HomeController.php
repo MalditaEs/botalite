797582c215b108dc8553f334ff2f26aea38682d3
David Fernández Sancho <dfernandez@maldita.es>
2024-12-10 13:20:46 +0100

    Implement Facebook OAuth integration and update dependencies
    Enhanced the onboarding process by adding Facebook OAuth logic to handle access token retrieval and subscribed apps functionality. Updated JavaScript SDK version in templates and modified the Composer configuration to include new libraries like 'async-aws/core', 'async-aws/ses', and 'symfony/amazon-mailer' for better email and AWS service integrations. The changes ensure a seamless connection to Facebook services and extend application capabilities in handling asynchronous tasks.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
+
     #[Route("/", name: "homepage")]
     public function index(): Response
     {
@@ -24,5 +31,46 @@ class HomeController extends AbstractController
     public function legal(): Response
     {
         return $this->render('legal.html.twig');
+    }
+
+    #[Route("/onboarding", name: "contact", methods: ["POST"])]
+    public function onboarding(Request $request, HttpClientInterface $httpClient): JsonResponse
+    {
+
+        $data = json_decode($request->getContent(), true);
+        $waba = $data['processData']['waba']['data']['waba_id'];
+
+        $url = 'https://graph.facebook.com/v21.0/oauth/access_token';
+
+        $queryParams = [
+            'client_id' => '621829865763918',
+            'client_secret' => 'a28c410c7ad48911eaf2cb995a60c658',
+            'code' => $data['processData']['code'],
+        ];
+
+        try {
+            $response = $httpClient->request('GET', $url, [
+                'query' => $queryParams
+            ]);
+
+            $content = $response->getContent();
+
+            $token = json_decode($content)->access_token;
+            $url = "https://graph.facebook.com/v21.0/{$waba}/subscribed_apps";
+            $headers = [
+                'Authorization' => 'Bearer ' . $token,
+            ];
+
+            $response = $httpClient->request('POST', $url, [
+                'headers' => $headers,
+            ]);
+
+            $content = $response->getContent();
+
+            return new JsonResponse(json_decode($content, true));
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+
     }
 }
